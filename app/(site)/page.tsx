dad3bd56125b8b0818a47392572dc0084971b7f3
maddy <modakverma02@gmail.com>
2023-09-06 21:20:26 +0530

    first commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,19 @@
+import Header from "@/components/Header"
+
+export default function Home() {
+  return (
+    <div className="
+    bg-neutral-900
+    text-green-500
+    rounded-lg
+    h-full
+    w-full
+    overflow-hidden
+    overflow-y-auto
+    ">
+      <Header>
+        
+      </Header>
+    </div>
+  )
+}
