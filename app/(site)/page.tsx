02cd3602138e7d4a35c219632c884fb4fa0f0078
maddy <modakverma02@gmail.com>
2023-10-10 19:46:51 +0530

    loading & errorHandling addition

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,7 +6,8 @@ import PageContent from "./components/PageContent";
 export const revalidate = 0;
 
 export default async function Home() {
-  const songs = await getSongs()
+  const songs = await getSongs();
+
   return (
     <div className="
     bg-neutral-900
